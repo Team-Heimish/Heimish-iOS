9136c7ed3f4faed7bafafdb91928574bf7c58473
wonseok <wurijang@gmail.com>
2021-05-09 19:13:33 +0900

    [style] Balloon Cell-background, font color change

--- Heimish-iOS/AiChatbot-iOS/Sources/Cells/StorageTableViewCell.swift
+++ Heimish-iOS/AiChatbot-iOS/Sources/Cells/StorageTableViewCell.swift
@@ -15,7 +15,12 @@ class StorageTableViewCell: UITableViewCell {
     @IBOutlet weak var emotionView: UIView!{
         didSet{
             emotionView.makeRounded(cornerRadius: 10.0)
-            emotionView.dropShadow(color: .deepGreen, offSet: CGSize(width: 0, height: 5), opacity: 0.4, radius: 3)
+            emotionView.dropShadow(color: .hemishGray, offSet: CGSize(width: 0, height: 4), opacity: 0.4, radius: 3)
+        }
+    }
+    @IBOutlet weak var entryBtn: UIButton!{
+        didSet{
+            entryBtn.roundCorners(cornerRadius: 10.0, maskedCorners:[ .layerMaxXMinYCorner, .layerMaxXMaxYCorner])
         }
     }
     @IBOutlet weak var happyLabel: UILabel!
